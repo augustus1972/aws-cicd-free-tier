ba3f9faf0900f1977f273f07e6cad30edf2fc661
Maciek Paciarski <augustus@interia.eu>
2026-07-07 22:06:31 +0100

    Fix: remove default index.html before deployment

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Aktualizacja pakietów i instalacja Apache (jeśli nie został zainstalowany)
 apt-get update -y
 apt-get install -y apache2
+
+# Usunięcie domyślnego pliku Apache, aby CodeDeploy mógł wgrać nasz plik index.html bez błędów
+if [ -f /var/www/html/index.html ]; then
+    rm -f /var/www/html/index.html
+fi
